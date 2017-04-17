4f2f9795f87cdc48281afaaa76f94a17acdf3a1b
kacsagit <katajona@gmail.com>
2017-04-18 00:01:09 +0200

    design

---
+++ app/src/main/java/com/example/kata/onlab/event/DeleteFriendEvent.java
@@ -0,0 +1,18 @@
+package com.example.kata.onlab.event;
+
+/**
+ * Created by Kata on 2017. 02. 24..
+ */
+
+public class DeleteFriendEvent<T> {
+        private T data;
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+}
---
+++ app/src/main/java/com/example/kata/onlab/network/Friend.java
@@ -0,0 +1,14 @@
+package com.example.kata.onlab.network;
+
+/**
+ * Created by Kata on 2017. 04. 17..
+ */
+
+public class Friend {
+    int friend;
+
+    public Friend(int friend_){
+        friend=friend_;
+    }
+}
+
